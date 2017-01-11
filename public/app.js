e5e465e5ab5e40d857a7a2a67393cc1fee822ba4
keighty <k80.leonard@gmail.com>
2017-01-10 20:08:58 -0800

    Begin refactor.

--- public/app.js
+++ public/app.js
@@ -1,14 +1,38 @@
+const KEY_POINT = 'key-point'
+const ROCK_REASON = 'rock-reason'
 
-const keyPointListSelector = '.key-points'
-const rockReasonSelector = '.rock-reasons'
-const keyPointForm = document.querySelector('.add-key-point')
-const keyPointList = document.querySelector(keyPointListSelector)
-const rockReasonForm = document.querySelector('.add-rock-reason')
-const rockReasonList = document.querySelector('rockReasonSelector')
-const rockReasons = JSON.parse(localStorage.getItem('rock-reasons')) || []
-populateList(rockReasons, rockReasonSelector)
+const sections = [KEY_POINT, ROCK_REASON]
+const [
+  keyPointList,
+  rockReasonList
+] = sections
+      .map(section => document.querySelector(`.${section}`))
 
-const keyPoints = JSON.parse(localStorage.getItem('key-points')) || []
+// const keyPointList = document.querySelector(keyPointListSelector)
+// const rockReasonList = document.querySelector('rockReasonListSelector')
+
+const [
+  keyPointForm,
+  rockReasonForm
+] = sections
+      .map(section => document.querySelector(`.add-${section}`))
+
+// const keyPointForm = document.querySelector('.add-key-point')
+// const rockReasonForm = document.querySelector('.add-rock-reason')
+
+const keyPointListSelector = `.${KEY_POINT}s`
+const rockReasonListSelector = `.${ROCK_REASON}s`
+
+const [
+  keyPoints,
+  rockReasons
+] = sections
+      .map(section => JSON.parse(localStorage.getItem(`${section}s`)))
+
+// const rockReasons = JSON.parse(localStorage.getItem('rock-reasons')) || []
+// const keyPoints = JSON.parse(localStorage.getItem('key-points')) || []
+
+populateList(rockReasons, rockReasonListSelector)
 populateList(keyPoints, keyPointListSelector)
 
 function addKeyPoint(e) {
@@ -36,7 +60,7 @@ function addRockReason(e) {
   e.preventDefault()
   const text = this.querySelector('[name]').value
   rockReasons.push(text)
-  populateList(rockReasons, rockReasonSelector)
+  populateList(rockReasons, rockReasonListSelector)
   localStorage.setItem('rock-reasons', JSON.stringify(rockReasons))
   this.reset()
 }
